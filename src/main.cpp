9f4e4f17f1b9bc4ba378aabeb758178c4bcf832b
RYBA <ryba@dispostable.com>
2017-11-07 01:00:39 +0300

    final step

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x384b060671f4a93948e9c168216dadb0ca2fbc54aa11c86b0345b6af1c59b2f5");
+uint256 hashGenesisBlock("0x2bdc12f658cb06b059628014e15dedc394291c2587cb17c5049b99ec37827ae2");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2032,9 +2032,9 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1509972097;
+        block.nTime    = 1509996767;
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 464947;
+        block.nNonce   = 6182331;
 
         if (fTestNet)
         {
@@ -2049,7 +2049,7 @@ bool LoadBlockIndex(bool fAllowNew)
         assert(block.hashMerkleRoot == uint256("0x7fe4a944d6693e91321a0e59055a88ceefdee98009736a4b54ff68b30f15baa4"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (true && block.GetHash() != hashGenesisBlock)
+        if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
